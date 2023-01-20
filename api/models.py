cb2a73266f4b58e4d91777cd8f52c4e338ba0675
johnson845173 <rebeirojohnson@gmail.com>
2023-01-20 23:07:06 +0530

    added new field

--- api/models.py
+++ api/models.py
@@ -2,9 +2,31 @@ from django.db import models
 
 # Create your models here.
 
-# class Task(models.Model):
-#     title = models.CharField(max_length=200)
-#     completed = models.BooleanField(default=False, blank=True, null=True)
-      
-#     def __str__(self):
-#         return self.title
+class Results(models.Model):
+    stockid = models.AutoField(primary_key=True)
+    stockname = models.CharField(max_length=50)
+    stratergy = models.CharField(default=None, max_length=100)
+    fullname = models.CharField(max_length=100)
+    entry_price = models.FloatField(default=None)
+    exit_price = models.FloatField(default=None)
+    entry_date = models.DateField(default=None)
+    exit_date = models.DateField(auto_now=True)
+    profit = models.FloatField(default=None)  
+    percentage = models.FloatField(default=None)
+
+    def __str__(self):
+        return self.title
+
+
+class Todays_pick(models.Model):
+    stockid = models.AutoField(primary_key=True)
+    stockname = models.CharField(max_length=50)
+    fullname = models.CharField(max_length=100)
+    entry_price = models.FloatField(default=None)
+    exit_price = models.FloatField(default=None)
+    time_frame = models.DateField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
